556828eeb80dd7e72a3e599e502545f6da0057d2
MatCrojas <95934914+MatCrojas@users.noreply.github.com>
2022-10-13 11:11:17 -0500

    Update: Url

--- 10b/practica6/js/app.js
+++ 10b/practica6/js/app.js
@@ -1,5 +1,5 @@
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('https://matcrojas.github.io/Aplicaciones-Web-Progresivas/10b/practica6/sw.js');
 } else {
     console.log('No se permite SW :(');
 }
