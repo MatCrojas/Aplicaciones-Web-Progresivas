a7575c3af9e9e496d5527070ac0d09b5ab3f4bcf
MatCrojas <95934914+MatCrojas@users.noreply.github.com>
2022-10-13 22:36:33 -0500

    Update: Url

--- 10b/practica4/sw.js
+++ 10b/practica4/sw.js
@@ -3,10 +3,10 @@ self.addEventListener('install', (event) => {
 
     const respCache = caches.open('cache-v1').then((cache) => {
         return cache.addAll([
-            '/',
-            '/index.html',
-            '/css/style.css',
-            '/js/app.js',
+            'https://matcrojas.github.io/Aplicaciones-Web-Progresivas/10b/practica4/',
+            'https://matcrojas.github.io/Aplicaciones-Web-Progresivas/10b/practica4/index.html',
+            'https://matcrojas.github.io/Aplicaciones-Web-Progresivas/10b/practica4/css/style.css',
+            'https://matcrojas.github.io/Aplicaciones-Web-Progresivas/10b/practica4/js/app.js',
             'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css',
             'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js'
         ]);
