b4ba198ba43e0b1c58691b36490c2f193d49a575
MatCrojas <95934914+MatCrojas@users.noreply.github.com>
2023-07-03 12:03:51 -0600

    Add files via upload

---
+++ 10b/practica11/js/app.js
@@ -0,0 +1,115 @@
+import { getNotesFireStore, saveNoteFireStore, updateNoteFireStore } from '/js/firestore-functions.js'
+
+//Contiene el maximo de notas en  la base de datos
+var maxNotes = 1;
+//Contiene el maximo de notas en pantalla
+var onPage = 0;
+//Contiene el limite de notas que traera cada que hace una consulta
+var limit = 5;
+
+//La n representa desde que numero del arreglo comienza a pintar las notas
+const getAllNotes = async (n) => {
+    const doc = await getNotesFireStore();
+    let card = document.getElementById('cards')
+    let newCard = document.createElement('div');
+
+    maxNotes = doc.size;
+
+    newCard.innerHTML = '';
+
+    doc.forEach((doc) => {
+        if (limit != 0 && onPage != maxNotes && n <= 0) {
+
+            let newCard = document.createElement('div');
+
+            newCard.innerHTML = `
+            <div class="card" id="note`+ onPage + `">
+            <div class="card-body" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <div class="row">
+                    <div class="col-2">
+                        <!-- La imagen de la nota, si la tiene -->
+                        <img src="https://blogs.unsw.edu.au/nowideas/files/2020/03/eventos-corporativos.jpg"
+                            class="img-fluid rounded" alt="...">
+                    </div>
+                    <div class="col text-truncate">` +
+                doc.data().text
+                + `</div>
+                </div>
+            </div>
+        </div>`
+            card.appendChild(newCard);
+            document.getElementById("note" + onPage).addEventListener("click", function () {
+                $('#exampleModal').modal({ show:true });
+                document.getElementById("nota-text").innerHTML = doc.data().text;
+            }, false);
+            limit--;
+            onPage++;
+        }
+
+        n--;
+    });
+    limit = 5;
+}
+
+const saveNote = async (note) => {
+    const result = await saveNoteFireStore(note);
+    if (result === 'ok') {
+        alert('Nota Registrada')
+
+    } else {
+        alert('Nota No Registrada')
+    }
+}
+
+const btnSaveNote = document.getElementById('btnSaveNote');
+btnSaveNote.addEventListener('click', async () => {
+    const textNote = document.getElementById('textNote');
+    const note = {
+        text: textNote.value
+    }
+
+    await saveNote(note)
+
+    textNote.value = '';
+
+    await getAllNotes(maxNotes + 1);
+
+});
+
+const updateNote = async (note) => {
+    const result = await updateNoteFireStore(note);
+    if (result === 'ok') {
+        alert('Nota Actualizada')
+
+    } else {
+        alert('Nota No Actualizada')
+    }
+}
+
+const btnUpdateNote = document.getElementById('btnUpdateNote');
+btnUpdateNote.addEventListener('click', async () => {
+    const textNote = document.getElementById('textNote');
+    const note = {
+        text: textNote.value
+    }
+
+    await updateNote(note)
+
+    textNote.value = '';
+
+    await getAllNotes(maxNotes + 1);
+
+});
+
+//Para funcionar con el scroll
+const onScroll = () => {
+    if (document.body.scrollHeight - window.innerHeight === window.scrollY) {
+        // hacer fetch
+        getAllNotes(5)
+    }
+}
+
+window.addEventListener('scroll', onScroll)
+
+getAllNotes(0);
+
---
+++ 10b/practica11/js/firestore-functions.js
@@ -0,0 +1,49 @@
+import { collection, addDoc, getDocs, setDoc, query, getFirestore } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js"; 
+
+
+import { app } from "./firestore.js";
+
+const db = getFirestore(app);
+
+const getNotesFireStore = async () => {
+    const q = query(collection(db, 'notes'));
+    const querySnapshot = await getDocs(q);
+    return querySnapshot
+    
+    /*
+    querySnapshot.forEach((doc) => {
+        console.log(doc.id, ' => ', doc.data());
+    });
+    */
+};
+
+const saveNoteFireStore = async (note) => {
+    const docRef = await addDoc(collection(db, 'notes'), {
+    text: note.text,
+    create_at: new Date()
+  });
+
+  if(docRef.id){
+    return 'ok'
+  }
+  return 'failed' 
+}
+
+//No tengo idea
+const updateNoteFireStore = async (note) => {
+  const docRef = await setDoc(collection(db, 'notes').doc('test').set({
+  text: note.text,
+  create_at: new Date()
+}));
+
+if(docRef.id){
+  return 'ok'
+}
+return 'failed' 
+}
+
+export{
+    getNotesFireStore,
+    saveNoteFireStore,
+    updateNoteFireStore
+}
